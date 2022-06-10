d6218929ec85964c0cdc445d1682ae2c1c51f6d0
Eduard Gimeno <eduard.gimeno@c0rps.com>
2022-06-10 10:26:04 +0200

    Testing

---
+++ main.tf
@@ -0,0 +1,19 @@
+provider "aws"{
+    region = "eu-west-1"
+}
+
+terraform {
+  backend "s3"{
+      bucket = "tfstateedudev"
+      key = "edu/devvpc/terraform.state"
+      region = "eu-west-1"
+      dynamodb_table = "tflockedudev"
+  }
+}
+
+resource "aws_s3_bucket" "this" {
+  bucket                               = "testingEduBitsAdev123"
+  force_destroy                        = "true"
+  region                               = "eu-west-1"
+
+}
