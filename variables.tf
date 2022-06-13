c5fb372839da6201ba4bf5b12d8d51604c398f2b
eddyhg97 <92509960+eddyhg97@users.noreply.github.com>
2022-06-13 14:50:07 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,3 +4,12 @@ variable "environment" {
 variable "project" {
   type = string
 }
+
+variable "folder_name" {
+  description = "S3 Folder name"
+  type = list(string)
+  default = ["folder1",
+             "folder1/in",
+             "folder1/out",
+             ]
+}
