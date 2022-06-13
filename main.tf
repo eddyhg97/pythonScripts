eb55186d4deaa65f70738b733bb8afe1efa8214d
eddyhg97 <92509960+eddyhg97@users.noreply.github.com>
2022-06-13 13:04:39 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-resource "aws_ecs_cluster" "cluster" {
-  name = "${var.project}-${var.environment}"
-  setting {
-    name  = "containerInsights"
-    value = "enabled"
-  }
-  capacity_providers = ["FARGATE", ]
+resource "aws_s3_bucket" "this_bucket" {
+  bucket = "demo_bucket_edu_test_bits_ade123"
+}
+
+resource "aws_s3_object" "object" {
+  bucket = aws_s3_bucket.this_bucket.id
+  key    = "demo/directory/"
 }
