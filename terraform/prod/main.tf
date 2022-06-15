acf26c762fbbddd5bccde8007c1890f9cf982d23
eddyhg97 <92509960+eddyhg97@users.noreply.github.com>
2022-06-15 15:41:06 +0200

    Update main.tf

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -18,5 +18,5 @@ resource "aws_s3_bucket" "this_bucket" {
 
 resource "aws_s3_object" "object" {
   bucket = aws_s3_bucket.this_bucket.id
-  key    = "demo/directory/folder1/folder2/"
+  key    = "demo/directory/folder1/folder2/FolderC/"
 }
