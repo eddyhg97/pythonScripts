958a0e68d6670d50f37e09659bb41a62e2845d0c
eddyhg97 <92509960+eddyhg97@users.noreply.github.com>
2022-06-15 10:02:29 +0200

    Update main.tf

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -1,17 +1,3 @@
-
-provider "aws" {
-  region = "eu-west-1"
-}
-
-terraform {
-  backend "s3"{
-      bucket = "tfstateedudev"
-      key = "edu/devvpc/prod/terraform.state"
-      region = "eu-west-1"
-      dynamodb_table = "tflockedudev"
-  }
-}
-
 resource "aws_s3_bucket" "this_bucket" {
   bucket = "gitactionseduadevintaprod"
 }
