0d70ba86c179977b5eab9d537bec9db1ab22d452
Eduard Gimeno <eduard.gimeno@c0rps.com>
2022-06-15 10:54:27 +0200

    Add

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -1,5 +1,19 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+terraform {
+  backend "s3"{
+      bucket = "tfstateedudev"
+      key = "edu/devvpc/terraform.state"
+      region = "eu-west-1"
+      dynamodb_table = "tflockedudev"
+  }
+}
+
+
 resource "aws_s3_bucket" "this_bucket" {
-  bucket = "gitactionseduadevintaprod"
+  bucket = "gitactionseduadevintaprod1"
 }
 
 resource "aws_s3_object" "object" {
