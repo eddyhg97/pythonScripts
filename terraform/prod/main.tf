20ea66c4e386f290875829db38937d125660ba5c
eddyhg97 <92509960+eddyhg97@users.noreply.github.com>
2022-08-31 13:50:52 +0200

    Update main.tf

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -18,5 +18,5 @@ resource "aws_s3_bucket" "this_bucket" {
 
 resource "aws_s3_object" "object" {
   bucket = aws_s3_bucket.this_bucket.id
-  key    = "demo/Adevinta"
+  key    = "demo/Adevinta_Spain"
 }
